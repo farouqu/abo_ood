3ca570b56458a23703f2aa311e873a2d4a733792
Farouq Umar <ufakz99@gmail.com>
2023-11-23 20:03:34 +0200

    chore: minor cleanup

--- utils/make_annotation_info.py
+++ utils/make_annotation_info.py
@@ -8,10 +8,10 @@ with open(file_path, "r") as read_file:
 image_info = data["images"]
 annotation_info = data["annotations"]
 
-# Create a dictionary to map image IDs to image paths
+
 image_id_to_path = {int(item["id"]): item["file_name"] for item in image_info if "file_name" in item}
 
-# Iterate through annotation data, add "image_path", and save in a new list
+
 new_annotation_data = []
 for item in annotation_info:
     if "image_id" in item:
@@ -20,7 +20,7 @@ for item in annotation_info:
             item["image_path"] = image_id_to_path[image_id]
             new_annotation_data.append(item)
 
-# Save the modified annotation data to a new file
+
 with open("data/AboShips/labels/train/annotation_info.json", "w") as file:
     json.dump(new_annotation_data, file, indent=2)
 
